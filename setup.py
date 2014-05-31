3b721f1626b76243e28a4a0edf44a612424c03d0
Cas Rusnov <rusnovn@gmail.com>
2014-05-31 00:51:01 -0700

    add setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='ppcode',
+      version='alpha1.1',
+      description='A simple filesystem based WSGI CMS',
+      url='https://gitorious.org/pixywerk/pixywerk/',
+      author='Cas Rusnov',
+      author_email='rusnovn@gmail.com',
+      license='MIT',
+      packages=['pixywerk'],
+      zip_safe=False)
